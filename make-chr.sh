54e4859b412b89652ffe454968a58857b06088bc
RafalSc007er <150515553+RafalSc007er@users.noreply.github.com>
2025-06-06 12:36:15 +0200

    Update make-chr.sh

--- make-chr.sh
+++ make-chr.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 #
-# Digital Ocean Ubuntu 24.10 x64 Droplet with "Regular Intel" CPU, 512 MB Memory / 10 GB Disk
-#
-# Running:
-# git clone https://github.com/RafalSc007er/CHR.git
-# cd CHR/
-# chmod +x make-chr.sh
-# ./make-chr.sh
-#
-# Once the reboot is done, login with root/CHANGEME and change the password!
-# You might get a "Segmentation fault".
-# Most of the time this is absolutely fine. Reboot the droplet and attempt to login using Winbox.
-# If it didn't work, just trash the droplet and try it again.
-#
-#
 wget https://download.mikrotik.com/routeros/7.19.1/chr-7.19.1.img.zip -O chr.img.zip  && \
 gunzip -c chr.img.zip > chr.img  && \
 apt-get update && \
